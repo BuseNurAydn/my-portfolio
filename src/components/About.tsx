e570ca0f0e24df8151d68ccaab542c3b7a9ab88d
BuseNurAydn <83844222+BuseNurAydn@users.noreply.github.com>
2025-01-07 21:33:22 +0300

    düzenleme

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -14,7 +14,7 @@ const About = () => {
     <section id='about' className='dark:bg-gray800-color scroll-mt-16'>
       <div className={`${containerStyle} relative overflow-auto md:overflow-visible`}>
         <motion.div
-          className="md:w-2/3 space-y-4 p-6 lg:p-0"
+          className="md:w-2/3 space-y-4 p-4 lg:p-0"
           animate={{ opacity: 1 }}
           initial="hidden"
           whileInView="visible"
